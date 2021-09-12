31e676e1f3d0b11b813c70ff440f62a4adedb16e
buihien <hien@techmaster.vn>
2021-09-13 01:27:34 +0700

    homework day 03

--- homework/03/main.go
+++ homework/03/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"hw03/calendar"
 	"hw03/docker"
+	"hw03/tree"
 )
 
 func main() {
+	// Bài 1
 	calendar.ShowCalendar(2021, 9)
-	docker.ListContainer()
+	fmt.Println()
+
+	// Bài 2
+	containers, _ := docker.ListContainer(docker.ALL)
+	for _, c := range containers {
+		fmt.Printf("%s - %s\n", c.Names[0], c.State)
+	}
+
+	_ = docker.StartContainerById("2926490de30f")
+	_ = docker.StopContainerById("7f86a07c6921")
+
+	fmt.Println()
+
+	// Bài 3
+	data := tree.PrintDirectoryTree(".")
+	fmt.Println(data)
 }
