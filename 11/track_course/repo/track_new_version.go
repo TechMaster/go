00101931a03cde76ef96103e1b8b2a9e834f62fc
techmaster <cuong@techmaster.vn>
2021-10-10 23:00:16 +0700

    track_course

--- 11/track_course/repo/track_new_version.go
+++ 11/track_course/repo/track_new_version.go
@@ -2,8 +2,12 @@ package repo
 
 /*
 Admin, Sales, Maintainer có thể thay đổi track, course mà không cập nhật version.
-Chỉ khi nào họ chủ định tạo ra một phiên bản mới new version mới
-thì phiên bản track mới, course mới sẽ đươc tạo ra clone từ track mới nhất
+Chỉ khi nào thực sự cần thiết họ mới tạo phiên bản mới của track
+Truyền vào track_master_id, tìm ra bản ghi có is_master = true, lấy được id của track, gọi là source_track_id
+Tạo một track mới có id mới gọi là target_track_id
+
+Select trong track_course có track_id bằng source_track_id
+Tạo mới tất cả các bản ghi này, thay source_track_id bằng target_track_id
 */
 
 func Clone_track_to_new_version(track_master_id string) (err error) {
